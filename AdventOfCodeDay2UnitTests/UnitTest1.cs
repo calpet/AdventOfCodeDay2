e2f4f9cb1afe9ae55cc8df4ecba8e9980d7cceca
Calin Peters <calinpeters98@hotmail.nl>
2020-12-16 12:37:27 +0100

    Added unit tests to validate requirements.

--- AdventOfCodeDay2UnitTests/UnitTest1.cs
+++ AdventOfCodeDay2UnitTests/UnitTest1.cs
@@ -7,20 +7,70 @@ namespace AdventOfCodeDay2UnitTests
     {
         private PasswordManager pwdManager;
         private AlgorithmManager algoManager;
+        private string[] passwords;
 
         [SetUp]
         public void Setup()
+        {
+        }
+
+        /// <summary>
+        /// Naming convention: [methodName]_[input]_[expectedOutput]()
+        /// </summary>
+
+        [Test]
+        public void GetPasswords_None_IsNotNull()
+        {
+            //Arrange
+            pwdManager = new PasswordManager();
+            
+            //Act:
+            var passwords = pwdManager.GetPasswords();
+
+            //Assert:
+            Assert.NotNull(passwords);
+        }
+
+        [Test]
+        public void SplitPasswordString_PasswordString_ArrayWith4Records()
+        {
+            //Arrange:
+            pwdManager = new PasswordManager();
+            string password = "9-12 q: qqqxhnhdmqqqqjz";
+
+            //Act:
+            var result = pwdManager.SplitPasswordString(password);
+            //Assert:
+            Assert.AreEqual(4, result.Length);
+        }
+
+        //569 & 346 are the correct answers for my day 2 problem, hence the specific outcome.
+        [Test]
+        public void StartFirstAssigment_AOCPasswordFile_569ValidPasswords()
         {
             //Arrange:
             pwdManager = new PasswordManager();
             algoManager = new AlgorithmManager();
 
+            //Act: 
+            int result = algoManager.StartFirstAssignment(pwdManager);
+
+            //Assert:
+            Assert.AreEqual(569, result);
         }
 
         [Test]
-        public void Test1()
+        public void StartSecondAssignment_AOCPasswordFile_342ValidPasswords()
         {
-            Assert.Pass();
+            //Arrange:
+            pwdManager = new PasswordManager();
+            algoManager = new AlgorithmManager();
+
+            //Act:
+            int result = algoManager.StartSecondAssignment(pwdManager);
+
+            //Assert:
+            Assert.AreEqual(346, result);
         }
     }
 }
